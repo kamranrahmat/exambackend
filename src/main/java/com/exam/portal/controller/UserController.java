9acdcf58705b20a92b5ce226d256773dcf01f637
SAPPHIRECS\Kamran.Rehmatullah <kamranrahmatullah33@gmail.com>
2022-06-22 17:39:42 +0500

    allow cross origin

--- src/main/java/com/exam/portal/controller/UserController.java
+++ src/main/java/com/exam/portal/controller/UserController.java
@@ -4,6 +4,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import com.exam.portal.service.UserService;
 
 @RestController
 @RequestMapping("/user")
+@CrossOrigin("*")
 public class UserController {
 
 	@Autowired
@@ -29,6 +31,9 @@ public class UserController {
 	public User createUser(@RequestBody User user) throws Exception {
 		
 		Set<UserRole> roles=new HashSet<UserRole>();
+		if (user.getProfile()== null || user.getProfile().equals("")) {
+			user.setProfile("default.png");
+		}
 		
 		Role role=new Role();
 		role.setRoleId(1L);
